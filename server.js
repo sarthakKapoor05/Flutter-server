eb1a12c6fbf8e150764d9b63b57a62283930c8c0
m-triple-m <triplem656@gmail.com>
2025-04-28 17:52:26 +0530

    added nodemon

--- server.js
+++ server.js
@@ -6,18 +6,33 @@ const wss = new WebSocket.Server({ port: 8080 });
 
 console.log("WebSocket server is running on ws://localhost:8080");
 
+// Track all connected clients
+const clients = new Set();
+
 wss.on("connection", function connection(ws) {
   console.log("A new client connected!");
+  
+  // Add the client to our set
+  clients.add(ws);
 
   // Receive message from Flutter
   ws.on("message", function incoming(message) {
     console.log("Received:", message.toString());
 
-    // Echo back the message to Flutter
+    // Echo back the message to the sender
     ws.send(`You said: ${message}`);
+    
+    // Broadcast to all other connected clients
+    clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(`Someone said: ${message}`);
+      }
+    });
   });
 
   ws.on("close", function close() {
+    // Remove client from set when they disconnect
+    clients.delete(ws);
     console.log("Client disconnected");
   });
 });
